288446f406bf8738d0f8f66b0a042b72a7cd7094
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-09-28 11:03:16 +0200

    taak02loopsfor

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+for ($x = 0; $x < 18; $x++) {
+  echo "Ik ben $x jaar, dus ik mag nog niet stemmen <br>";
+}
+echo "Ik ben 18 jaar oud dus ik heb stemrecht!";
+?>  
+
+</body>
+</html>
