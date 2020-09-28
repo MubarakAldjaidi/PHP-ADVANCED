11e0ab92d342342a123fac285ed0f6600b4add70
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-09-28 10:55:12 +0200

    taak01loops

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$leeftijd = 0;
+
+    while($leeftijd < 18){
+        echo "Ik ben $leeftijd, dus ik mag nog niet stemmen <br/>";
+        $leeftijd++;
+} 
+?>  
+
+</body>
+</html>
