213a06bad6e94b9b1e0419fc7e2d5286b7690764
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-09-28 12:08:49 +0200

    mijnmuziek

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="mijngegevens.php" method="get">
+Favoriete Artiest: <input type="text" name="name"><br>
+Titel: <input type="text" name="email"><br>
+Jaar van uitkomst: <input type="text" name="email"><br>
+Muzieksoort: <input type="text" name="email"><br>
+
+<input type="submit">
+</form>
+<!---- http://localhost/phpadvanced/mijngegevens.php?name=popsmoke&email=&email=&email=
+
+</body>
+</html>
