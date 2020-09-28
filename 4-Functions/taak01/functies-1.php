f73a87180bee7420b23e713c39294823b75799bc
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-09-28 12:22:20 +0200

    functies-1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function schrijfKleur($skleur) {
+  echo "Deze tekst is in de kleur $skleur <br>";
+}
+
+schrijfKleur("Red");
+schrijfKleur("Blue");
+schrijfKleur("Purple");
+schrijfKleur("Green");
+schrijfKleur("Brown");
+?>
+
+</body>
+</html>
